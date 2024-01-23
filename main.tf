a78d3c437b63a623e153ee1194cc729d6ed3925e
veerlaajay <148520501+veerlaajay@users.noreply.github.com>
2024-01-23 13:25:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -100,7 +100,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
  
  admin_ssh_key {
     username   = "linuxadmin"
-    public_key = file("id_rsa.pub")
+    password = "Ajay@123456789"
   }
 os_disk {
     caching              = "ReadWrite"
